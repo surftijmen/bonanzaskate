9bbe837e31da7cf505f78d1717b32cd2689eb088
Tijmen Meijer <t.g.meijer@student.tudelft.nl>
2022-04-13 11:10:31 +0200

    difficulty cards

--- js/jsonReader.js
+++ js/jsonReader.js
@@ -2,28 +2,39 @@ fetch("recourses/tricks.json")
 .then(response => response.json())
 .then(data => {
 
-    let trick = data.tricks[(Math.floor(Math.random() * data.tricks.length))];
+    for (let i = 0; i < 3; i++) {
 
-    let h4 = document.getElementById("trick");
-    h4.innerText = trick.name;
+        let tricks = [];
 
-    let image = document.getElementById("image");
-    image.src = trick.image;
+        data.tricks.forEach(t => {
+            if (t.points == i+1){
+                tricks.push(t);
+            }
+        });
 
-    let color = "rgb(139, 224, 209)";
-    let points = "Points: 2";
+        let trick = tricks[(Math.floor(Math.random() * tricks.length))];
 
-    if (trick.points == 2) {
-        color = "rgb(169, 189, 136)";
-        points = "Points: 1";
-    } else if (trick.points == 3) {
-        color = "rgb(217, 119, 89)";
-        points = "Points: 3";
+        let h4 = document.getElementById("trick"+i);
+        h4.innerText = trick.name;
+
+        let image = document.getElementById("image"+i);
+        image.src = trick.image;
+
+        let color = "rgb(139, 224, 209)";
+        let points = "Points: 1";
+
+        if (trick.points == 2) {
+            color = "rgb(169, 189, 136)";
+            points = "Points: 2";
+        } else if (trick.points == 3) {
+            color = "rgb(217, 119, 89)";
+            points = "Points: 3";
+        }
+
+        document.getElementById("points"+i).innerText = points;
+
+        let h = document.getElementById("card"+i);
+        h.style.backgroundColor = color;
     }
-
-    document.getElementById("points").innerText = points;
-
-    let h = document.getElementById("card");
-    h.style.backgroundColor = color;
 })    
 
