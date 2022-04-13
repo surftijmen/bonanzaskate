bdfe3db0ce4f8a54f949a9c40d8443448b24e506
Tijmen Meijer <t.g.meijer@student.tudelft.nl>
2022-04-13 10:59:52 +0200

    add recourses

--- js/jsonReader.js
+++ js/jsonReader.js
@@ -4,22 +4,26 @@ fetch("recourses/tricks.json")
 
     let trick = data.tricks[(Math.floor(Math.random() * data.tricks.length))];
 
-    let h4 = document.createElement("h4");
+    let h4 = document.getElementById("trick");
     h4.innerText = trick.name;
-    
-    let h = document.getElementById("card");
-    h.appendChild(h4);
 
     let image = document.getElementById("image");
     image.src = trick.image;
 
-    let color = "rgb(155, 102, 102)";
+    let color = "rgb(139, 224, 209)";
+    let points = "Points: 2";
+
     if (trick.points == 2) {
-        color = "rgb(1, 102, 102)";
+        color = "rgb(169, 189, 136)";
+        points = "Points: 1";
     } else if (trick.points == 3) {
-        color = "rgb(255, 102, 102)";
+        color = "rgb(217, 119, 89)";
+        points = "Points: 3";
     }
 
-    h.style.backgroundColor = color
+    document.getElementById("points").innerText = points;
+
+    let h = document.getElementById("card");
+    h.style.backgroundColor = color;
 })    
 
