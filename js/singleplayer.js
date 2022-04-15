b2a6fdf056f5e7d378d7db2f9da19af2752f417a
Tijmen Meijer <t.g.meijer@student.tudelft.nl>
2022-04-15 17:23:42 +0200

    add back to card

--- js/singleplayer.js
+++ js/singleplayer.js
@@ -1,6 +1,6 @@
 initializeCards = function() {
     // get all the cards
-    let cards = document.getElementsByClassName("card");
+    let cards = document.getElementsByClassName("flip-card-inner");
     
     // store all card's rotations in an array
     let rotation = Array(cards.length).fill(0);
@@ -10,7 +10,15 @@ initializeCards = function() {
         cards[i].onclick = function() {
             // rotate 180 degrees 
             rotation[i] = (rotation[i] + 180) % 360;
+            cards[i].style.transition = 'transform 0.5s 0.0s'
             cards[i].style.transform = 'rotateY('+rotation[i]+'deg)'; 
+            for (let j = 0; j < cards.length; j++) {
+                if (i!=j){
+                    rotation[j] = 0;
+                    cards[j].style.transition = 'transform 0.5s 0.2s'
+                    cards[j].style.transform = 'rotateY('+0+'deg)'; 
+                }
+            }
         }
     }
 }
