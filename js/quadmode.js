1626e38d26563480a9b3bfd245ee0fc0667a0642
Tijmen Meijer <t.g.meijer@student.tudelft.nl>
2022-04-18 12:44:35 +0200

    implement life system

--- js/quadmode.js
+++ js/quadmode.js
@@ -22,28 +22,37 @@ function initialize() {
 
     for (let i = 1; i < cards.length; i++) {
         cards[i].onclick = function() {
+            // if green turn red
             if (cards[i].style.backgroundImage == "linear-gradient(rgb(67, 150, 88), rgb(53, 133, 67))"){
                 cards[i].style.backgroundImage = "linear-gradient(rgb(211, 71, 71), rgb(208, 37, 37))";
-            } else{
-                if (cards[i].style.backgroundImage == "linear-gradient(rgb(166, 166, 166), rgb(126, 126, 126))") {
-                    cards[i].style.backgroundImage = 'linear-gradient(rgb(67, 150, 88), rgb(53, 133, 67))';
-                } else {
-                    for (let i = 1; i < 5; i++) {
-                        cards[i].style.backgroundImage = "linear-gradient(rgb(166, 166, 166), rgb(126, 126, 126))";
-                    }
-
-                    allTricks.pop(cards[i].children[0].innerHTML);
-
-                    currentTrick = cards[i].children[0].innerHTML;
-
-                    cards[1].children[0].innerHTML = "Regular";
-                    cards[2].children[0].innerHTML = "Switch";
-                    cards[3].children[0].innerHTML = "Nollie";
-                    cards[4].children[0].innerHTML = "Fakie";
-
-                    cards[0].children[0].innerHTML = currentTrick;
-
+            } else {
+                // if red turn green
+                if (cards[i].style.backgroundImage == "linear-gradient(rgb(211, 71, 71), rgb(208, 37, 37))") {
+                    cards[i].style.backgroundImage = "linear-gradient(rgb(67, 150, 88), rgb(53, 133, 67))"
                 }
+            }
+
+            // if gray turn green
+            if (cards[i].style.backgroundImage == "linear-gradient(rgb(166, 166, 166), rgb(126, 126, 126))") {
+                cards[i].style.backgroundImage = 'linear-gradient(rgb(67, 150, 88), rgb(53, 133, 67))';
+            }
+            
+            // if blue turn all gray
+            if (cards[i].style.backgroundImage == "linear-gradient(rgb(92, 174, 241), rgb(20, 130, 173))" 
+                || cards[i].style.backgroundImage == "") {
+
+                for (let i = 1; i < 5; i++) {
+                    cards[i].style.backgroundImage = "linear-gradient(rgb(166, 166, 166), rgb(126, 126, 126))";
+                }
+
+                allTricks.pop(cards[i].children[0].innerHTML);
+                currentTrick = cards[i].children[0].innerHTML;
+                
+                cards[0].children[0].innerHTML = currentTrick;
+                cards[1].children[0].innerHTML = "Regular";
+                cards[2].children[0].innerHTML = "Switch";
+                cards[3].children[0].innerHTML = "Nollie";
+                cards[4].children[0].innerHTML = "Fakie";
             }
         }
     }
@@ -68,6 +77,10 @@ function saveAndQuit() {
     }
 }
 
+function endGame() {
+    
+}
+
 function finishRound() {
     
     let cards = document.getElementsByClassName("card");
@@ -81,9 +94,27 @@ function finishRound() {
 
     if (generateNewTrick) {
         achievedTricks.push(currentTrick);
+        currentTrick = null;
+        console.log(achievedTricks);
+
+        for (let i = 1; i < 5; i++) {
+            if (cards[i].style.backgroundImage = "linear-gradient(rgb(92, 174, 241), rgb(20, 130, 173))") {
+                generateNewTrick = false;
+            }
+        }
+
         document.getElementById("score").innerText = "Score: " + achievedTricks.length;
         setTricks();
     } else {
         health--;
+
+        if (health > 0 && health <= 6) {
+            document.getElementById("health").src = "../recourses/healthbar/" + health + "hp.png";
+        }
+
+        if (health <= 0) {
+            endGame();
+            restartGame();
+        }
     }
 }
